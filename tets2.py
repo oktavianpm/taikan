a5bf625a894f9ab3937205078567cf03ac1c6cca
Oktavian Putra Masyiakh <78895409+oktavianpm@users.noreply.github.com>
2023-05-13 16:28:28 +0700

    Update tets2.py

--- tets2.py
+++ tets2.py
@@ -95,22 +95,3 @@ print(result.inserted_ids)
 # Close the connection to MongoDB when you're done.
 
 client.close()
-
-
-
-
-####################
-
-# from pymongo.mongo_client import MongoClient
-# from pymongo.server_api import ServerApi
-# # Set the Stable API version when creating a new client
-# client = MongoClient(uri, server_api=ServerApi('1'))
-                          
-# # Send a ping to confirm a successful connection
-# try:
-#     client.admin.command('ping')
-#     print("Pinged your deployment. You successfully connected to MongoDB!")
-# except Exception as e:
-#     print(e)
-
-#####################################
