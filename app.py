a387cf8843dbfcbe5a01adbe5c3c72f10fb467b6
Oktavian Putra Masyiakh <78895409+oktavianpm@users.noreply.github.com>
2023-05-15 22:17:07 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,93 @@
+import streamlit as st
+import pandas as pd
+import pymongo
+
+from pymongo import MongoClient
+from streamlit_option_menu import option_menu  # pip install streamlit-option-menu
+from PIL import Image
+
+# import datetime for date fields
+from datetime import datetime
+datetime_now = datetime.now() # pass this to a MongoDB doc
+
+im = Image.open("favicon.ico")
+st.set_page_config(
+    page_title='SmartFishSense',
+    page_icon=im,
+    layout='wide', #centered or wide
+    initial_sidebar_state='expanded',
+)
+
+st.title("SmartFishSense")
+
+# --- NAVIGATION MENU ---
+selected = option_menu(
+    menu_title=None,
+    options=["Control Menu", "Recent Status"],
+    icons=["bi bi-toggles2", "bi bi-hourglass-split"],  # https://icons.getbootstrap.com/
+    orientation="horizontal",
+)
+
+# Replace the uri string with your MongoDB deployment's connection string.
+
+uri = "mongodb+srv://agapedsky:dagozilla@cluster0.ro1gcoc.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(uri)
+db = client.TA
+coll = db.data1
+# coll.drop()
+
+col1, col2 = st.columns(2)
+
+#Inisialisasi awal
+resetx = 0
+exitx = 0
+statusx = 0
+confidencex = 0
+
+def add_data():
+    client = MongoClient(uri)
+    db = client.TA
+    coll = db.data1
+    # coll.drop()
+
+    timex = datetime.today()
+    docs = [
+            {"Date": (timex.strftime("%x")),"Time":  (timex.strftime("%X")), "Reset": resetx, "Exit_idle": exitx, "Status": statusx, "Confidence": confidencex,},
+            ]
+    return docs
+
+def load_data():
+    client = MongoClient(uri)
+    db = client.TA
+    coll = db.data1
+    # coll.drop()
+    x = coll.find()
+    df = pd.DataFrame(x)
+    selected_columns = ['Date','Time','Status','Confidence']
+    df_selected = df[selected_columns]
+    return df_selected
+
+if selected == "Control Menu":
+    with col1:
+        if st.button('Reset'):
+            resetx = 1
+        else:
+            resetx = 0
+    with col2:
+        if st.button('Exit Idle'):
+            exitx = 1
+        else:
+            exitx = 0
+    if exitx != 0 or resetx != 0:
+        if exitx == 1 and resetx ==1:
+            exitx = 0
+            resetx = 1
+        data_input = add_data()
+        result = coll.insert_many(data_input)
+
+if selected == "Recent Status":
+    data_status = load_data()
+    st.table(data_status)
+    
+client.close()
